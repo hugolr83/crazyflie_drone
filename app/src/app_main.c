9987a6cc6d07c76e21a83ee0941552d880f1080f
sami <sami.sadfa@polymtl.ca>
2021-09-27 15:09:31 -0400

    add app layer

---
+++ app/src/app_main.c
@@ -0,0 +1,64 @@
+/**
+ * ,---------,       ____  _ __
+ * |  ,-^-,  |      / __ )(_) /_______________ _____  ___
+ * | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+ * | / ,--´  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+ *    +------`   /_____/_/\__/\___/_/   \__,_/ /___/\___/
+ *
+ * Crazyflie control firmware
+ *
+ * Copyright (C) 2020 Bitcraze AB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * api_app.c - App layer application that calls app API functions to make
+ *             sure they are compiled in CI.
+ */
+
+
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "app.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "debug.h"
+
+#include "ledseq.h"
+#include "crtp_commander_high_level.h"
+#include "locodeck.h"
+#include "mem.h"
+#include "log.h"
+#include "param.h"
+#include "pm.h"
+#include "app_channel.h"
+#include "system.h"
+
+
+#define DEBUG_MODULE "APPAPI"
+
+void appMain() {
+  // LED sequencer
+  {
+    ledseqContext_t ledSeqContext;
+    ledseqRegisterSequence(&ledSeqContext);
+    ledseqRun(&ledSeqContext);
+    ledseqRunBlocking(&ledSeqContext);
+    ledseqStop(&ledSeqContext);
+    ledseqStopBlocking(&ledSeqContext);
+  }
+}
