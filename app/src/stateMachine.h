9e3f9170e2e4cc12e961103dbf9fc80b889334db
sami <sami.sadfa@polymtl.ca>
2021-10-20 17:12:32 -0400

    chore : forgot file (precedent commit)

---
+++ app/src/stateMachine.h
@@ -0,0 +1,9 @@
+#ifndef SRC_STATEMACHINE_H_
+#define SRC_STATEMACHINE_H_
+#include "models.h"
+#include "led_app.h"
+#include "communication_unit.h"
+
+void handleCommand(Command command);
+
+#endif
